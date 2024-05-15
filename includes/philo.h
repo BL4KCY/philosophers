9a170e46ff4759770138394ad283415efdc0f34f
BL4KCY <mostafafarssiui@gmail.com>
2024-05-15 19:55:27 +0100

    chore: Update philo.h with additional fields and functions

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:19:15 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/12 16:45:20 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/15 16:20:53 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include <unistd.h>
+# include <stdbool.h>
 # include <sys/time.h>
 
 
@@ -28,6 +30,8 @@ typedef struct s_data
 	int				time_to_sleep;
 	int				nb_must_eat;
 	int				philo_must_eat;
+	bool			dead;
+	pthread_t		death_thread;
 	pthread_mutex_t	*forks;
 	pthread_mutex_t	print;
 	pthread_mutex_t	death;
@@ -38,6 +42,8 @@ typedef struct s_philo
 {
 	int				id;
 	pthread_t		thread;
+	long			last_meal;
+	pthread_mutex_t	death;
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 	struct s_data	*data;
@@ -48,5 +54,11 @@ typedef struct s_philo
 int		init_data(t_data *data, int ac, char **av);
 int		init_philo(t_data *data);
 void	*routine(void *philo);
+long	get_time(t_data *data);
+void	print_msg(t_data *data, int id, char *msg);
+void	*check_death(void *philo);
+void	get_fork(t_philo *p);
+void	drop_fork(t_philo *p);
+void	ft_usleep(long time);
 
 #endif
