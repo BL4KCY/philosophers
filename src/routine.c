42d76561d599e0d1095bf901bbc7cdb8fa01a0e6
BL4KCY <mostafafarssiui@gmail.com>
2024-05-15 19:55:53 +0100

    Refactor routine function to track the number of times a philosopher must eat

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 16:43:26 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/12 16:46:22 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/15 09:55:03 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 void	*routine(void *philo)
 {
 	t_philo	*p;
+	int		i;
 
 	p = (t_philo *)philo;
-	while (1)
+	i = 0;
+	while (i != p->data->nb_must_eat && !p->data->dead)
 	{
-		pthread_mutex_lock(&p->data->death);
-		pthread_mutex_unlock(&p->data->death);
+		get_fork(p);
+		drop_fork(p);
+		if (p->data->nb_must_eat != -1)
+			++i;
 	}
 	return (NULL);
 }
