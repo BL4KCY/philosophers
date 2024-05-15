442048404be5bfc135a25790aa91f1f5d17e2183
BL4KCY <mostafafarssiui@gmail.com>
2024-05-15 19:55:38 +0100

    feat: Update init.c with improved error handling and thread management

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 16:33:05 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/12 16:42:26 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/15 18:36:05 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 int	init_data(t_data *data, int ac, char **av)
 {
-	data->nb_philo = ft_atoi(av[1]);
-	data->time_to_die = ft_atoi(av[2]);
-	data->time_to_eat = ft_atoi(av[3]);
-	data->time_to_sleep = ft_atoi(av[4]);
+	data->nb_philo = atoi(av[1]);
+	data->time_to_die = atoi(av[2]);
+	data->time_to_eat = atoi(av[3]);
+	data->time_to_sleep = atoi(av[4]);
 	data->nb_must_eat = -1;
+	data->dead = false;
 	if (ac == 6)
-		data->nb_must_eat = ft_atoi(av[5]);
+		data->nb_must_eat = atoi(av[5]);
 	data->philo_must_eat = 0;
-	if (data->nb_philo < 2 || data->time_to_die < 60 || data->time_to_eat < 60
-		|| data->time_to_sleep < 60 || (ac == 6 && data->nb_must_eat < 1))
-		return (printf("Error: Invalid arguments\n"), 1);
 	data->forks = malloc(sizeof(pthread_mutex_t) * data->nb_philo);
 	if (!data->forks)
 		return (printf("Error: malloc failed\n"), 1);
 	pthread_mutex_init(&data->print, NULL);
 	pthread_mutex_init(&data->death, NULL);
+	for (int i = 0; i < data->nb_philo; i++)
+		pthread_mutex_init(&data->forks[i], NULL);
 	gettimeofday(&data->start, NULL);
 	return (0);
 }
@@ -52,12 +52,11 @@ int	init_philo(t_data *data)
 	}
 	i = -1;
 	while (++i < data->nb_philo)
-	{
 		if (pthread_create(&philo[i].thread, NULL, &routine, &philo[i]))
-			return (printf("Error: pthread_create failed\n"), 1);
-	}
-	i = -1;
-	while (++i < data->nb_philo)
-		pthread_join(philo[i].thread, NULL);
+			return (write(2, "Error: pthread_create failed\n", 30), 1);
+	if (pthread_create(&data->death_thread, NULL, check_death, philo))
+		return (write(2, "Error: pthread_create failed\n", 30), 1);
+	if (pthread_join(data->death_thread, NULL))
+		return (write(2, "Error: pthread_join failed\n", 27), 1);
 	return (0);
 }
