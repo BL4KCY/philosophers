e40a274dc1ab4f9f6ddb033098ad1a138e372ed8
BL4KCY <mostafafarssiui@gmail.com>
2024-05-15 19:55:44 +0100

    chore: Update main.c with usage instructions for command line arguments

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:19:55 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/12 16:37:27 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/14 18:17:10 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@ int main(int ac, char **av)
 	t_data data;
 
 	if (ac != 5 && ac != 6 && av)
-		return (printf("Error: Wrong number of arguments\n"), 1);
+		return (printf("usage: ./philo <nb_philo> <time_to_die> <time_to_eat> <time_to_sleep> [nb_must_eat]\n"), 0);
 	if (init_data(&data, ac, av))
 		return (1);
 	if (init_philo(&data))
 		return (1);
+	return (0);
 }
