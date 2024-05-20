edf7f301c76ebf7347768d182cf1582e6c36c278
BL4KCY <mostafafarssiui@gmail.com>
2024-05-20 16:01:20 +0100

    Refactor print_msg function to handle philosopher death and return status

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:07:45 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/15 18:24:31 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/20 15:58:24 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,21 @@ long	get_time(t_data *data)
 	return ((time.tv_sec * 1000) + (time.tv_usec / 1000) - (data->start.tv_sec * 1000) - (data->start.tv_usec / 1000));
 }
 
-void	print_msg(t_data *data, int id, char *msg)
+int	print_msg(t_data *data, int id, char *msg)
 {
+	pthread_mutex_lock(&data->death);
 	pthread_mutex_lock(&data->print);
 	if (!data->dead || !strcmp(msg, "died"))
 		printf("%ld %d %s\n", get_time(data), id, msg);
+	if (!strcmp(msg, "died"))
+	{
+		pthread_mutex_unlock(&data->print);
+		pthread_mutex_unlock(&data->death);
+		return (1);
+	}
 	pthread_mutex_unlock(&data->print);
+	pthread_mutex_unlock(&data->death);
+	return (0);
 }
 
 void	*check_death(void *philo)
@@ -36,40 +45,54 @@ void	*check_death(void *philo)
 	p = (t_philo *)philo;
 	while (1)
 	{
+		pthread_mutex_lock(&p->data->death);
 		if (get_time(p->data) - p->last_meal > p->data->time_to_die)
 		{
-			p->data->dead = true;
+			for (int i = 0; i < p->data->nb_philo; i++)
+				p->data->philo[i].data->dead = true;
+			pthread_mutex_unlock(&p->data->death);
 			print_msg(p->data, p->id, "died");
+			ft_usleep(100);
 			return (NULL);
 		}
+		pthread_mutex_unlock(&p->data->death);
 	}
 	return (NULL);
 }
 
-void	get_fork(t_philo *p)
+int		get_fork(t_philo *p)
 {
 	if (p->id % 2)
 		pthread_mutex_lock(p->right_fork);
 	else
 		pthread_mutex_lock(p->left_fork);
-	print_msg(p->data, p->id, "has taken a fork");
+	if (print_msg(p->data->philo->data, p->id, "has taken a fork"))
+		return (1);
 	if (p->id % 2)
 		pthread_mutex_lock(p->left_fork);
 	else
 		pthread_mutex_lock(p->right_fork);
-	print_msg(p->data, p->id, "has taken a fork");
+	if (print_msg(p->data->philo->data, p->id, "has taken a fork"))
+		return (1);
+	return (0);
 }
 
-void	drop_fork(t_philo *p)
+int	drop_fork(t_philo *p)
 {
+	pthread_mutex_lock(&p->data->death);
 	p->last_meal = get_time(p->data);
-	print_msg(p->data, p->id, "is eating");
+	pthread_mutex_unlock(&p->data->death);
+	if (print_msg(p->data->philo->data, p->id, "is eating"))
+		return (1);
 	ft_usleep(p->data->time_to_eat);
 	pthread_mutex_unlock(p->left_fork);
 	pthread_mutex_unlock(p->right_fork);
-	print_msg(p->data, p->id, "is sleeping");
+	if (print_msg(p->data->philo->data, p->id, "is sleeping"))
+		return (1);
 	ft_usleep(p->data->time_to_sleep);
-	print_msg(p->data, p->id, "is thinking");
+	if (print_msg(p->data->philo->data, p->id, "is thinking"))
+		return (1);
+	return (0);
 }
 
 void	ft_usleep(long ms)
