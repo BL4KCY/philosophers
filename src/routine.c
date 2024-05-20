80b02b78486372dfb02d165badfeab698df7516d
BL4KCY <mostafafarssiui@gmail.com>
2024-05-20 16:01:05 +0100

    feat: Fix deadlock issue in routine function

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: melfersi <melfersi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 16:43:26 by melfersi          #+#    #+#             */
-/*   Updated: 2024/05/15 09:55:03 by melfersi         ###   ########.fr       */
+/*   Updated: 2024/05/20 10:11:50 by melfersi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,18 @@ void	*routine(void *philo)
 
 	p = (t_philo *)philo;
 	i = 0;
+	pthread_mutex_lock(&p->data->death);
 	while (i != p->data->nb_must_eat && !p->data->dead)
 	{
-		get_fork(p);
-		drop_fork(p);
+		pthread_mutex_unlock(&p->data->death);
+		if (get_fork(p))
+			return (NULL);
+		if (drop_fork(p))
+			return (NULL);
 		if (p->data->nb_must_eat != -1)
 			++i;
+		pthread_mutex_lock(&p->data->death);
 	}
+	pthread_mutex_unlock(&p->data->death);
 	return (NULL);
 }
